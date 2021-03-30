18e0d63c95a0bb58f9774ee7dcfcdd500db637cb
Nikhil Benesch <nikhil.benesch@gmail.com>
2021-03-30 15:07:54 -0400

    Enable Sentry error reporting on the frontend
    Also fix release handling for the backend.

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,20 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import { UserProvider } from "./auth/AuthContext";
 import config from "./config";
+
+if (config.sentryDsn) {
+  Sentry.init({
+    dsn: config.sentryDsn,
+    environment: config.sentryEnvironment,
+    release: config.sentryRelease,
+    integrations: [new Integrations.BrowserTracing()],
+  });
+}
 
 const root = document.createElement("div");
 document.body.appendChild(root);
