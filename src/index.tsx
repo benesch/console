53436d6c6585d8d837dbafeeceb7e2f1b84e6342
Andreas Fuchs <asf@materialize.com>
2021-09-02 12:07:51 -0400

    Add temporary workaround for frontegg losing defaults
    These values' nullability got changed on the type definition, so we
    have to provide them ourselves in order to succeed the build.

--- src/index.tsx
+++ src/index.tsx
@@ -44,7 +44,20 @@ ReactDOM.render(
   <React.StrictMode>
     <FronteggProvider
       contextOptions={{ baseUrl: config.fronteggUrl }}
-      authOptions={{ routes: { authenticatedUrl: "/deployments" } }}
+      authOptions={{
+        routes: {
+          authenticatedUrl: "/deployments",
+          // These URLs need to be specified due to a missing default in frontegg.
+          // TODO: remove when upgraded to frontegg 3.0.2+
+          loginUrl: "/account/login",
+          logoutUrl: "/account/logout",
+          activateUrl: "/account/activate",
+          acceptInvitationUrl: "/account/invitation/accept",
+          forgetPasswordUrl: "/account/forget-password",
+          resetPasswordUrl: "/account/reset-password",
+          signUpUrl: "/account/sign-up",
+        },
+      }}
       headerImage={logo}
     >
       <AuthedFetchProvider>
