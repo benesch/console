1216bd45534471ef498db57581dede878d6edbbe
Tony Rippy <tonyrippy@gmail.com>
2021-10-21 16:51:43 -0400

    Adjusts footer text and test expectations so they match.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -24,7 +24,7 @@ export const PageFooter: React.FC = () => {
       fontSize="sm"
       boxShadow="footer"
     >
-      © {getCurrentYear()} Materialize.
+      © {getCurrentYear()} Materialize, Inc.
       <Link
         href="https://materialize.com/privacy-policy"
         target="_blank"
