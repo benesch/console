0fb4f97ddfaf08fb80eb88f6837521b3cd3f8dac
Alex Hunt <alex.hunt@materialize.com>
2021-11-09 16:23:08 -0800

    Audit logging using django-simple-history
    This unfortunately has three different sources for the "history_user_id":
    1. An APIUser from a normal web request (ie: the frontend).
    2. A django auth User from the admin portal.
    3. An environment variable for the deployment controller, which doesn't
       have a user of either type.

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -46,6 +46,30 @@ export interface DeploymentRequest {
 
 export type DeploymentSizeEnum = "XS" | "S" | "M" | "L" | "XL";
 
+export interface HistoricalDeployment {
+  id: string;
+  organization: string | null;
+  tlsAuthority: string | null;
+  name: string;
+  hostname: string | null;
+  flaggedForDeletion: boolean;
+  flaggedForUpdate: boolean;
+  size: DeploymentSizeEnum;
+  storageMb: number;
+  disableUserIndexes: boolean;
+  materializedExtraArgs: string[];
+  clusterId: string | null;
+  mzVersion: string;
+  pendingMigration: string | null;
+  enableTailscale: boolean;
+  cloudProviderRegion: SupportedCloudRegion;
+  historyDate: string;
+  historyChangeReason: string | null;
+  historyType: HistoryTypeEnum;
+}
+
+export type HistoryTypeEnum = "+" | "~" | "-";
+
 export interface OnboardingCall {
   start: string;
   end: string;
@@ -371,6 +395,57 @@ export const useDeploymentsCertsRetrieve = ({
   useGet<string, unknown, void, DeploymentsCertsRetrievePathParams>(
     (paramsInPath: DeploymentsCertsRetrievePathParams) =>
       `/api/deployments/${paramsInPath.id}/certs`,
+    { pathParams: { id }, ...props }
+  );
+
+export interface DeploymentsHistoryListPathParams {
+  id: string;
+}
+
+export type DeploymentsHistoryListProps = Omit<
+  GetProps<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryListPathParams;
+
+export const DeploymentsHistoryList = ({
+  id,
+  ...props
+}: DeploymentsHistoryListProps) => (
+  <Get<HistoricalDeployment[], unknown, void, DeploymentsHistoryListPathParams>
+    path={`/api/deployments/${id}/history`}
+    {...props}
+  />
+);
+
+export type UseDeploymentsHistoryListProps = Omit<
+  UseGetProps<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryListPathParams;
+
+export const useDeploymentsHistoryList = ({
+  id,
+  ...props
+}: UseDeploymentsHistoryListProps) =>
+  useGet<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >(
+    (paramsInPath: DeploymentsHistoryListPathParams) =>
+      `/api/deployments/${paramsInPath.id}/history`,
     { pathParams: { id }, ...props }
   );
 
