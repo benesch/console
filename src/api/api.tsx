1734c6b9c7c3ddf81590befe915a5dfba2670997
Alex Hunt <alex.hunt@materialize.com>
2021-11-18 16:57:35 -0800

    audit log diff api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -46,6 +46,74 @@ export interface DeploymentRequest {
 
 export type DeploymentSizeEnum = "XS" | "S" | "M" | "L" | "XL";
 
+export interface HistoricalDeployment {
+  id: string;
+  organization: string | null;
+  tlsAuthority: string | null;
+  name: string;
+  hostname: string | null;
+  flaggedForDeletion: boolean;
+  flaggedForUpdate: boolean;
+  size: DeploymentSizeEnum;
+  storageMb: number;
+  disableUserIndexes: boolean;
+  materializedExtraArgs: string[];
+  clusterId: string | null;
+  mzVersion: string;
+  pendingMigration: string | null;
+  enableTailscale: boolean;
+  cloudProviderRegion: SupportedCloudRegion;
+  historyDate: string;
+  historyUserName: string;
+  historyType: HistoryTypeEnum;
+}
+
+export interface HistoricalDeploymentChange {
+  name?: ModelChangeChar;
+  hostname?: ModelChangeChar;
+  flaggedForDeletion?: ModelChangeBoolean;
+  flaggedForUpdate?: ModelChangeBoolean;
+  size?: ModelChangeSize;
+  disableUserIndexes?: ModelChangeBoolean;
+  materializedExtraArgs?: ModelChangeListChar;
+  clusterId?: ModelChangeChar;
+  mzVersion?: ModelChangeChar;
+  enableTailscale?: ModelChangeBoolean;
+}
+
+export interface HistoricalDeploymentDelta {
+  changes: HistoricalDeploymentChange;
+  metadata: HistoricalDeploymentMetadata;
+}
+
+export interface HistoricalDeploymentMetadata {
+  historyDate: string;
+  historyUserName: string;
+  historyType: HistoryTypeEnum;
+}
+
+export type HistoryTypeEnum = "+" | "~" | "-";
+
+export interface ModelChangeBoolean {
+  old: boolean | null;
+  new: boolean | null;
+}
+
+export interface ModelChangeChar {
+  old: string | null;
+  new: string | null;
+}
+
+export interface ModelChangeListChar {
+  old: string | null[];
+  new: string | null[];
+}
+
+export interface ModelChangeSize {
+  old: DeploymentSizeEnum;
+  new: DeploymentSizeEnum;
+}
+
 export interface OnboardingCall {
   start: string;
   end: string;
@@ -379,6 +447,113 @@ export const useDeploymentsCertsRetrieve = ({
   useGet<string, unknown, void, DeploymentsCertsRetrievePathParams>(
     (paramsInPath: DeploymentsCertsRetrievePathParams) =>
       `/api/deployments/${paramsInPath.id}/certs`,
+    { pathParams: { id }, ...props }
+  );
+
+export interface DeploymentsHistoryListPathParams {
+  id: string;
+}
+
+export type DeploymentsHistoryListProps = Omit<
+  GetProps<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryListPathParams;
+
+export const DeploymentsHistoryList = ({
+  id,
+  ...props
+}: DeploymentsHistoryListProps) => (
+  <Get<HistoricalDeployment[], unknown, void, DeploymentsHistoryListPathParams>
+    path={`/api/deployments/${id}/history`}
+    {...props}
+  />
+);
+
+export type UseDeploymentsHistoryListProps = Omit<
+  UseGetProps<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryListPathParams;
+
+export const useDeploymentsHistoryList = ({
+  id,
+  ...props
+}: UseDeploymentsHistoryListProps) =>
+  useGet<
+    HistoricalDeployment[],
+    unknown,
+    void,
+    DeploymentsHistoryListPathParams
+  >(
+    (paramsInPath: DeploymentsHistoryListPathParams) =>
+      `/api/deployments/${paramsInPath.id}/history`,
+    { pathParams: { id }, ...props }
+  );
+
+export interface DeploymentsHistoryDiffListPathParams {
+  id: string;
+}
+
+export type DeploymentsHistoryDiffListProps = Omit<
+  GetProps<
+    HistoricalDeploymentDelta[],
+    unknown,
+    void,
+    DeploymentsHistoryDiffListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryDiffListPathParams;
+
+export const DeploymentsHistoryDiffList = ({
+  id,
+  ...props
+}: DeploymentsHistoryDiffListProps) => (
+  <Get<
+    HistoricalDeploymentDelta[],
+    unknown,
+    void,
+    DeploymentsHistoryDiffListPathParams
+  >
+    path={`/api/deployments/${id}/history_diff`}
+    {...props}
+  />
+);
+
+export type UseDeploymentsHistoryDiffListProps = Omit<
+  UseGetProps<
+    HistoricalDeploymentDelta[],
+    unknown,
+    void,
+    DeploymentsHistoryDiffListPathParams
+  >,
+  "path"
+> &
+  DeploymentsHistoryDiffListPathParams;
+
+export const useDeploymentsHistoryDiffList = ({
+  id,
+  ...props
+}: UseDeploymentsHistoryDiffListProps) =>
+  useGet<
+    HistoricalDeploymentDelta[],
+    unknown,
+    void,
+    DeploymentsHistoryDiffListPathParams
+  >(
+    (paramsInPath: DeploymentsHistoryDiffListPathParams) =>
+      `/api/deployments/${paramsInPath.id}/history_diff`,
     { pathParams: { id }, ...props }
   );
 
