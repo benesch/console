18f2355fde771f4ba621e95433e541af77a56dc5
Timothee CLAIN <tim@materialize.com>
2021-12-14 17:02:05 -0500

    feat: add backend middleware to notice sentry when incompatible clients are trying to access the api

--- src/version/api.ts
+++ src/version/api.ts
@@ -1,6 +1,6 @@
 import { isValidString } from "../utils/validators";
 
-export const currentVersion = window.CONFIG.sentryRelease;
+export const currentVersion = window.CONFIG?.sentryRelease;
 
 export const versionHeaders = (): Record<string, string> =>
   isValidString(currentVersion)
