371039460a93978701b9edd8c2af8572e58f558f
Karen Rustad Tolva <karen@materialize.com>
2021-12-15 21:15:23 -0800

    Add act() to Copyable tests

--- src/components/Copyable.test.tsx
+++ src/components/Copyable.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react";
+import { act, fireEvent, render } from "@testing-library/react";
 import React from "react";
 
 import { CopyableText } from "./Copyable";
@@ -16,12 +16,14 @@ describe("Copyable", () => {
   jest.useFakeTimers();
   monkeyPatchClipboard();
   afterEach(jest.resetAllMocks);
-  it("should copy the inner text in the clipboard on click", () => {
+  it.only("should copy the inner text in the clipboard on click", () => {
     const { getByTestId } = render(
       <CopyableText data-testid="copyable">Hello World</CopyableText>
     );
 
-    fireEvent.click(getByTestId("copyable"));
+    act(() => {
+      fireEvent.click(getByTestId("copyable"));
+    });
     expect(navigator.clipboard.writeText).toHaveBeenCalledWith("Hello World");
   });
 
@@ -31,11 +33,13 @@ describe("Copyable", () => {
     );
 
     expect(getByTestId("copyable-copyicon")).toBeDefined();
-    fireEvent.click(getByTestId("copyable"));
+    act(() => {
+      fireEvent.click(getByTestId("copyable"));
+    });
 
     await findByTestId("copyable-checkicon");
 
-    jest.advanceTimersByTime(1100);
+    // jest.advanceTimersByTime(1100);
     expect(findByTestId("copyable-copyicon")).toBeDefined();
   });
 });
