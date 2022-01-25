e1dc874b8354f7d8afdac8160850fa3a65336842
Tony Rippy <tonyrippy@gmail.com>
2022-01-24 23:53:45 -0500

    bin/fmt

--- src/deployments/detail/integrations/tailscale/EnableEditTailscaleConfiguration.tsx
+++ src/deployments/detail/integrations/tailscale/EnableEditTailscaleConfiguration.tsx
@@ -24,74 +24,75 @@ const validationSchema = Yup.object().shape({
   tailscaleAuthKey: Yup.string().required("Required"),
 });
 
-const EnableEditTailscaleConfiguration: React.FC<DeploymentIntegrationCallToActionProps> =
-  (props) => {
-    const { modalState, save, defaultValues, operation } =
-      useTailscaleIntegration(props);
+const EnableEditTailscaleConfiguration: React.FC<
+  DeploymentIntegrationCallToActionProps
+> = (props) => {
+  const { modalState, save, defaultValues, operation } =
+    useTailscaleIntegration(props);
 
-    return (
-      <>
-        <EnableEditTailscaleButton
-          onOpen={modalState.onOpen}
-          isEnabled={props.enabled}
-        />
-        <Modal isOpen={modalState.isOpen} onClose={modalState.onClose}>
-          <ModalOverlay />
-          <ModalContent data-testid="tailscale-configuration-modal">
-            <Formik
-              initialValues={defaultValues}
-              validationSchema={validationSchema}
-              onSubmit={(values) => save(values)}
-            >
-              <Form>
-                <ModalHeader flexGrow={1}>Configure Tailscale</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
+  return (
+    <>
+      <EnableEditTailscaleButton
+        onOpen={modalState.onOpen}
+        isEnabled={props.enabled}
+      />
+      <Modal isOpen={modalState.isOpen} onClose={modalState.onClose}>
+        <ModalOverlay />
+        <ModalContent data-testid="tailscale-configuration-modal">
+          <Formik
+            initialValues={defaultValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => save(values)}
+          >
+            <Form>
+              <ModalHeader flexGrow={1}>Configure Tailscale</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <VStack>
                   <VStack>
-                    <VStack>
-                      <TextField
-                        type="password"
-                        name="tailscaleAuthKey"
-                        label="Tailscale Auth Key"
-                      ></TextField>
-                      <Text fontSize="sm">
-                        See the{" "}
-                        <Link
-                          href="https://tailscale.com/kb/1085/auth-keys/"
-                          target="_blank"
-                          textDecoration="underline"
-                        >
-                          Tailscale documentation
-                        </Link>{" "}
-                        to learn how to generate a pre-authentication key.
-                      </Text>
-                    </VStack>
-                    {operation.error && (
-                      <UpdateIntegrationError name="Tailscale" />
-                    )}
+                    <TextField
+                      type="password"
+                      name="tailscaleAuthKey"
+                      label="Tailscale Auth Key"
+                    ></TextField>
+                    <Text fontSize="sm">
+                      See the{" "}
+                      <Link
+                        href="https://tailscale.com/kb/1085/auth-keys/"
+                        target="_blank"
+                        textDecoration="underline"
+                      >
+                        Tailscale documentation
+                      </Link>{" "}
+                      to learn how to generate a pre-authentication key.
+                    </Text>
                   </VStack>
-                </ModalBody>
-                <ModalFooter>
-                  <HStack>
-                    <Button
-                      onClick={modalState.onClose}
-                      size="sm"
-                      disabled={operation.loading}
-                    >
-                      Cancel
-                    </Button>
-                    <SubmitButton size="sm" disabled={operation.loading}>
-                      Save
-                    </SubmitButton>
-                  </HStack>
-                </ModalFooter>
-              </Form>
-            </Formik>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  };
+                  {operation.error && (
+                    <UpdateIntegrationError name="Tailscale" />
+                  )}
+                </VStack>
+              </ModalBody>
+              <ModalFooter>
+                <HStack>
+                  <Button
+                    onClick={modalState.onClose}
+                    size="sm"
+                    disabled={operation.loading}
+                  >
+                    Cancel
+                  </Button>
+                  <SubmitButton size="sm" disabled={operation.loading}>
+                    Save
+                  </SubmitButton>
+                </HStack>
+              </ModalFooter>
+            </Form>
+          </Formik>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 const EnableEditTailscaleButton: React.FC<{
   onOpen: () => void;
