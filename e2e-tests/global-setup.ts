5650724a0207bc9930d5ede6350f018528ea3bed
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-04-08 23:17:06 -0400

    e2e-tests: bump initial timeout to two minutes
    As far as I can tell, e2e tests flake in CI occasionally on PR builds
    because it can take longer for Webpack to build the bundle than the
    default 30s navigation timeout allows for. Bump that initial timeout to
    2 minutes to give Webpack more of a chance to succeed.

--- e2e-tests/global-setup.ts
+++ e2e-tests/global-setup.ts
@@ -5,7 +5,9 @@ import { CONSOLE_ADDR, EMAIL, PASSWORD, STATE_NAME } from "./util";
 export default async function globalSetup() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto(CONSOLE_ADDR);
+  // Wait up to two minutes for the page to become available initially, as
+  // Webpack can take a while to compile in CI.
+  await page.goto(CONSOLE_ADDR, { timeout: 1000 * 60 * 2 });
   await page.type("[name=email]", EMAIL);
   await page.press("[name=email]", "Enter");
   await page.waitForSelector("[name=password]"); // wait for animation
