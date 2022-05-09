392fd6c5c24f7cd4a31e4087f27d86a03da4618a
Andreas Fuchs <asf@materialize.com>
2022-05-09 17:14:20 -0400

    Prettier!!!!

--- e2e-tests/platform.spec.ts
+++ e2e-tests/platform.spec.ts
@@ -122,7 +122,9 @@ async function connectRegionPostgres(
   password: string,
   row: Locator
 ): Promise<Client> {
-  await row.locator("td >> text=/^postgres:/").waitFor({timeout: 10 * 60 * 1000});
+  await row
+    .locator("td >> text=/^postgres:/")
+    .waitFor({ timeout: 10 * 60 * 1000 });
 
   const fields = row.locator("td");
   const url = new URL(await fields.nth(1).innerText());
