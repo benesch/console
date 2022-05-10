e14c2c63124f2dbe429c39f7d4b1349a73a3ed86
Joaquin Colacci <joaquincolacci@gmail.com>
2022-05-10 15:41:27 +0200

    feat(frontend): responsive editor

---
+++ src/platform/editor/code/index.tsx
@@ -0,0 +1,67 @@
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/theme/monokai.css";
+import "codemirror/theme/panda-syntax.css";
+import "codemirror/mode/sql/sql";
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/sql-hint";
+import "codemirror/addon/hint/show-hint.css";
+
+import { Box, Button } from "@chakra-ui/react";
+import React, { useCallback, useState } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+
+interface Props {
+  handleQuery: (query: string) => void;
+}
+
+const Code = (props: Props): JSX.Element => {
+  const { handleQuery } = props;
+
+  /**
+   * State
+   */
+  const [query, setQuery] = useState<string>("");
+
+  /**
+   * Handlers
+   */
+  const onClick = useCallback(() => {
+    handleQuery(query);
+  }, [handleQuery, query]);
+
+  const onBeforeChange = useCallback((editor, data, value) => {
+    setQuery(value);
+  }, []);
+
+  return (
+    <Box width={"100%"} position="relative" overflow={"scroll"}>
+      <Button
+        height="1.75rem"
+        size="sm"
+        onClick={onClick}
+        position="absolute"
+        zIndex={10}
+        top={2}
+        right={2}
+        backgroundColor={"green.700"}
+        _hover={{
+          backgroundColor: "green.500",
+        }}
+      >
+        Run
+      </Button>
+      <CodeMirror
+        value={query}
+        onBeforeChange={onBeforeChange}
+        options={{
+          mode: "text/x-pgsql",
+          theme: "material",
+          lineNumbers: true,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default Code;
---
+++ src/platform/editor/index.tsx
@@ -0,0 +1,152 @@
+import { Box, Flex, useToast } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
+
+import { useSql } from "../../api/materialized";
+import { Card } from "../../components/cardComponents";
+import { BaseLayout } from "../../layouts/BaseLayout";
+import Code from "./code";
+import Schema from "./schema";
+import Table from "./table";
+
+/**
+ * Column rendering for text
+ * @returns a function that creates a JSX.Elemet
+ */
+function columnCell(): (cellData: any) => JSX.Element {
+  return function (cellData: any) {
+    return (
+      <Box
+        maxWidth={150}
+        whiteSpace="nowrap"
+        textOverflow={"ellipsis"}
+        overflow={"hidden"}
+        fontWeight={300}
+      >
+        {cellData.value}
+      </Box>
+    );
+  };
+}
+
+interface State {
+  rows: Array<any>;
+  columns: Array<string>;
+}
+
+const GridLayout = (): JSX.Element => {
+  /**
+   * States
+   */
+  const [query, setQuery] = useState<string>();
+  const { data, error } = useSql(query);
+  const [{ rows, columns }, setState] = useState<State>({
+    rows: [],
+    columns: [],
+  });
+  const ref = useRef(null);
+
+  /**
+   * Hooks
+   */
+  const toast = useToast();
+
+  /**
+   * Effects
+   */
+  useEffect(() => {
+    if (data) {
+      const { rows: dataRows, columns: dataColumns } = data;
+      const tableColumns = (dataColumns || []).map((column, index) => ({
+        Header: column,
+        accessor: (originalRow: any) => originalRow[index],
+        id: column + index,
+        Cell: columnCell(),
+      }));
+
+      setState({
+        rows: dataRows || [],
+        columns: tableColumns as any,
+      });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (error) {
+      /**
+       * Capitalize Materialize's error message
+       */
+      const capError =
+        error.length > 0 && error.charAt(0).toUpperCase() + error.slice(1);
+
+      toast({
+        title: `Ouch. ${capError}`,
+        status: "error",
+      });
+    }
+  }, [error]);
+
+  /**
+   * Handlers
+   * @param newQuery Query to run agains Materialize
+   */
+  const handleQuery = (newQuery: string) => {
+    setQuery(newQuery);
+  };
+
+  return (
+    <BaseLayout overflow={"scroll"}>
+      <Flex height={"100%"} gap={5}>
+        <Card
+          ref={ref}
+          key={"schema"}
+          data-grid={{
+            h: 50,
+            i: "schema",
+            w: 2,
+            x: 0,
+            y: 0,
+          }}
+          overflow={"scroll"}
+          background={"purple.900"}
+          width="20%"
+          minWidth={"20%"}
+          height={"100%"}
+        >
+          <Schema />
+        </Card>
+        <Flex flexDirection={"column"} flex={1} gap={2} width="100%">
+          <Card
+            key={"editor"}
+            data-grid={{
+              h: 25,
+              i: "editor",
+              w: 9.5,
+              x: 2,
+              y: 0,
+            }}
+            overflow="hidden"
+          >
+            <Code handleQuery={handleQuery} />
+          </Card>
+          <Card
+            key={"table"}
+            data-grid={{
+              h: 25,
+              i: "table",
+              w: 9.5,
+              x: 2,
+              y: 2,
+            }}
+            overflow="hidden"
+          >
+            <Box overflow={"scroll"} background={"purple.900"}>
+              <Table columns={columns} rows={rows} />
+            </Box>
+          </Card>
+        </Flex>
+      </Flex>
+    </BaseLayout>
+  );
+};
+
+export default GridLayout;
