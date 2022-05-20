4f0f9e4e78b7193f8cde75573c0d7d91aca36eb9
Andreas Fuchs <asf@materialize.com>
2022-05-20 18:14:05 -0400

    Bump materialized docker tag to a version that has a stable UID

--- src/platform/environments/EnableEnvironmentModal.tsx
+++ src/platform/environments/EnableEnvironmentModal.tsx
@@ -38,7 +38,7 @@ interface Props extends ButtonProps {
 /// value whenever you wish to try new materialized functions in
 /// cloud.
 // TODO: Use something like the release tracks we use for materialize cloud deployments.
-const ImageTag = "unstable-69d1f3a1b31c89c519c6c2e4ed799ba5226f5188";
+const ImageTag = "unstable-45fdf4aef2f1561c697a5afdd8d3524fcc701bef";
 
 const EnableEnvironmentModal = (props: Props) => {
   const { refetch, isAdmin, ...buttonProps } = props;
