c9248f59119cb63b5a9391b201256a317613a91e
Brennan Vincent <brennan@umanwizard.com>
2022-06-08 14:52:38 -0400

    appease linter

--- src/org/Org.tsx
+++ src/org/Org.tsx
@@ -5,7 +5,12 @@ import { useAuth } from "../api/auth";
 
 const Org = () => {
   const { user } = useAuth();
-  if (user?.tenantId == null || user.email == null || !(user.email.endsWith("@materialize.com"))) return null;
+  if (
+    user?.tenantId == null ||
+    user.email == null ||
+    !user.email.endsWith("@materialize.com")
+  )
+    return null;
   return (
     <Tag size="sm" variant="outline" opacity={0.8} fontSize="80%">
       Org ID {user.tenantId}
