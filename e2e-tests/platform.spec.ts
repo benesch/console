02ee45a56f5c8f99246410df672545533dacf019
Joaquin Colacci <joaquincolacci@gmail.com>
2022-07-08 17:58:44 +0200

    fix(e2etest) multiregion

--- e2e-tests/platform.spec.ts
+++ e2e-tests/platform.spec.ts
@@ -3,6 +3,7 @@ import assert from "assert";
 import CacheableLookup from "cacheable-lookup";
 import { Client } from "pg";
 
+import { sleep } from "../frontend/src/util";
 import { CONSOLE_ADDR, EMAIL, IS_KIND, STATE_NAME, TestContext } from "./util";
 
 /**
@@ -25,18 +26,39 @@ test(`connecting to the environment controller`, async ({ page, request }) => {
   const password = `mzp_${clientId}${secret}`;
   await page.goto(`${CONSOLE_ADDR}/platform`);
 
+  // close welcome modal
+  const welcomeModalOpen = await page.$("[aria-label=Close]");
+  if (welcomeModalOpen) {
+    welcomeModalOpen.click();
+  }
+
   await page.waitForSelector("table tbody tr");
   const regionRows = page.locator("table tbody tr");
+  const regionsNames = [];
 
   for (let i = 0; i < (await regionRows.count()); i++) {
     const row = regionRows.nth(i);
+    const regionName = await row.locator("td").first().innerText();
+    regionsNames.push(regionName);
+
     await row.locator('button:text("Enable region")').click();
   }
 
   await page.click("[aria-label='Close']");
 
-  await testPlatformEnvironment(page, request, password);
+  for (const regionName of regionsNames) {
+    await page.selectOption("[aria-label='Environment']", {
+      label: regionName,
+    });
+    sleep(500);
+    await testPlatformEnvironment(page, request, password);
+  }
 });
+
+async function exitWelcomeModal(page: Page) {
+  await page.click('[aria-label="Close"]');
+  await page.click('button:has-text("Enable Region")');
+}
 
 async function testPlatformEnvironment(
   page: Page,
@@ -68,65 +90,6 @@ async function testPlatformEnvironment(
   assert.equal(indexCount, 1);
   return;
 }
-
-/**
- * Here lies the old platform environment testing that is currently not working.
- */
-// async function testPlatformEnvironment(
-//   page: Page,
-//   request: APIRequestContext,
-//   password: string
-// ) {
-//   const client = await connectRegionPostgres(page, password);
-//   await client.query("CREATE CLUSTER c REPLICAS (r1 (size 'xsmall'));");
-//   await client.query("SET CLUSTER = c");
-//   if (!IS_KIND) {
-//     // This S3 bucket lives in the "Materialize Sample Data" AWS account and is
-//     // managed in the i2 repository.
-//     await client.query(`CREATE MATERIALIZED SOURCE engagement
-//       FROM S3 DISCOVER OBJECTS MATCHING 'engagement.csv'
-//       USING BUCKET SCAN 'materialize-sample-data'
-//       WITH (
-//           role_arn = 'arn:aws:iam::137301051720:role/sample-data-reader',
-//           region = 'us-east-1'
-//       )
-//       FORMAT CSV WITH HEADER (id, status, active_time);`);
-//   } else {
-//     // In Minikube, we won't have access to the S3 bucket, so just create a
-//     // table with the expected contents. This still tests that the cluster
-//     // can be created and perform computation.
-//     await client.query(
-//       "CREATE TABLE engagement (id text, status text, active_time text, mz_record integer)"
-//     );
-//     await client.query(
-//       `INSERT INTO engagement VALUES
-//         ('9999', 'active', '8 hours', 1),
-//         ('888', 'inactive', '', 2),
-//         ('777', 'active', '3 hours', 3)
-//       `
-//     );
-//   }
-//   // Try reading from the source repeatedly to give it time to populate. This
-//   // won't be necessary once the following issue is resolved:
-//   // https://github.com/MaterializeInc/materialize/issues/11048
-//   for (let i = 0; i < 30; i++) {
-//     try {
-//       const result = await client.query(
-//         "SELECT id, status, active_time FROM engagement ORDER BY mz_record"
-//       );
-//       assert.deepStrictEqual(result.rows, [
-//         { id: "9999", status: "active", active_time: "8 hours" },
-//         { id: "888", status: "inactive", active_time: "" },
-//         { id: "777", status: "active", active_time: "3 hours" },
-//       ]);
-//       return;
-//     } catch (error) {
-//       console.log(error);
-//       await page.waitForTimeout(1000);
-//     }
-//   }
-//   throw new Error("source never contained expected records");
-// }
 
 async function connectRegionPostgres(
   page: Page,
