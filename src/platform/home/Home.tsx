cc368a8dc433a8865c191ca30693b2051312e7db
Karen Rustad Tolva <karen@materialize.com>
2022-07-28 13:14:40 -0700

    Nav bar and title changes
    Remove title from home page
    
    Remove beta text from nav bar and fix some of the responsiveness while
    we're at it.

--- src/platform/home/Home.tsx
+++ src/platform/home/Home.tsx
@@ -1,24 +1,12 @@
-import {
-  Box,
-  HStack,
-  Spacer,
-  Text,
-  useColorModeValue,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, VStack } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
 
 import useEnvironmentState from "../../api/useEnvironmentState";
 import { Card, CardContent, CardHeader } from "../../components/cardComponents";
-import {
-  PageBreadcrumbs,
-  PageHeader,
-  PageHeading,
-} from "../../layouts/BaseLayout";
+import { PageBreadcrumbs } from "../../layouts/BaseLayout";
 import EnvironmentSelectField from "../../layouts/EnvironmentSelect";
 import { currentEnvironment } from "../../recoil/environments";
-import { semanticColors } from "../../theme/colors";
 import ConnectSteps from "./ConnectSteps";
 import PasswordStep from "./PasswordStep";
 import StarterEnvironmentModal from "./StarterEnvironmentModal";
@@ -30,33 +18,9 @@ const Home = () => {
     current?.environmentControllerUrl
   );
 
-  const grayText = useColorModeValue(
-    semanticColors.grayText.light,
-    semanticColors.grayText.dark
-  );
-
   return (
     <>
       <PageBreadcrumbs />
-      <PageHeader>
-        <HStack
-          spacing={4}
-          alignItems="center"
-          justifyContent="flex-start"
-          width="100%"
-        >
-          <VStack alignItems="flex-start">
-            <PageHeading>Welcome to Materialize!</PageHeading>
-            <Text fontSize="md" textColor={grayText}>
-              Region:{" "}
-              {current
-                ? `${current?.provider}/${current?.region}`
-                : "No region active"}
-            </Text>
-          </VStack>
-          <Spacer />
-        </HStack>
-      </PageHeader>
       <VStack spacing={6} mb={6}>
         {environmentStatus === "Enabled" && (
           <>
