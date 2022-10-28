bbebf51bb5adc86675008256e72e56a186697546
Robin Clowers <robin.clowers@gmail.com>
2022-10-28 16:47:08 -0700

    Add useApiFetch

---
+++ src/api/useApiFetch.ts
@@ -0,0 +1,36 @@
+import { ApiError } from "openapi-typescript-fetch";
+import React from "react";
+
+export const useApiFetch = <T>({
+  apiFn,
+  lazy = false,
+}: {
+  apiFn: () => Promise<T>;
+  lazy?: boolean;
+}) => {
+  const [error, setError] = React.useState<ApiError | undefined>(undefined);
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState<T | undefined>();
+  const refetch = React.useCallback(async () => {
+    setLoading(true);
+    try {
+      const result = await apiFn();
+      setData(result);
+    } catch (e) {
+      const error = e as ApiError;
+      setError(error);
+    }
+    setLoading(false);
+  }, []);
+  React.useEffect(() => {
+    if (lazy) return;
+
+    refetch();
+  }, []);
+  return {
+    data,
+    loading,
+    error,
+    refetch,
+  };
+};
