7a903de1e7b12ca24bacfe9291ab6fc75c9e528b
Nikhil Benesch <nikhil.benesch@gmail.com>
2022-10-29 17:28:46 -0400

    Remove CSP when running frontend locally
    Configuring CSP when running the frontend locally entails quite a bit of
    complexity and doesn't seem to buy us anything. I think, in theory, it
    could be useful for debugging the production CSP configuration, but in
    practice it is different enough that you need to do CSP debugging on a
    personal stack anyway.

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,14 +1,12 @@
 import "webpack-dev-server";
 
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
-import builder from "content-security-policy-builder";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import fs from "fs";
 import path from "path";
 import { DefinePlugin } from "webpack";
 import { merge } from "webpack-merge";
 
-import getCspPolicy from "./csp";
 import base, { IDefinePluginOptions } from "./webpack.config";
 
 function loadFronteggUrl(): string {
@@ -20,9 +18,6 @@ function loadFronteggUrl(): string {
   return JSON.stringify(outputData["frontegg_url"]);
 }
 
-const appHostname = process.env.APP_HOSTNAME || "staging.cloud.materialize.com";
-const provisionHostname =
-  process.env.PROVISION_HOSTNAME || "staging.materialize.cloud";
 const DefinePluginOptions: IDefinePluginOptions = {
   __FRONTEGG_URL__: DefinePlugin.runtimeValue(loadFronteggUrl, [
     path.resolve("../config/settings/local.outputs.json"),
@@ -45,54 +40,6 @@ const backendHostUrl = backendHostname.startsWith("http")
   ? backendHostname
   : `http://${backendHostname}`;
 
-const reportUrl = new URL(
-  "https://o561021.ingest.sentry.io/api/5699757/security/?sentry_key=13c8b3a8d1e547c9b9493de997b04337"
-);
-reportUrl.searchParams.append(
-  "sentry_environment",
-  DefinePluginOptions.__SENTRY_ENVIRONMENT__
-);
-reportUrl.searchParams.append(
-  "sentry_release",
-  DefinePluginOptions.__SENTRY_RELEASE__
-);
-
-const cspPolicy = getCspPolicy({
-  fronteggUrl: JSON.parse(loadFronteggUrl()) as string,
-  statusPageId: JSON.parse(DefinePluginOptions.__STATUSPAGE_ID__) as string,
-  appHostname: "localhost:3000",
-});
-
-if (
-  ["true", "yes", "1"].includes(
-    (process.env.FIREFOX_DEV ?? "false").toLowerCase()
-  )
-) {
-  // Allow React devtools to work in Firefox
-  //  See: https://github.com/facebook/react/issues/17997
-  //       https://bugzilla.mozilla.org/show_bug.cgi?id=1267027
-  cspPolicy["script-src"].push("'unsafe-inline'");
-}
-
-cspPolicy["connect-src"].push(
-  "http://localhost:8001",
-  "http://localhost:8002",
-  "http://invalid:8002",
-  // kind IPs on macOS and Linux, for access to `materialized` running in
-  //  an environment.
-  "http://127.0.0.1:*",
-  `https://ec.0.us-east-1.aws.${appHostname}`,
-  `https://ec.0.eu-west-1.aws.${appHostname}`,
-  `https://rc.us-east-1.aws.${appHostname}`,
-  `https://rc.eu-west-1.aws.${appHostname}`,
-  `http://*.us-east-1.aws.${provisionHostname}:443`,
-  `http://*.eu-west-1.aws.${provisionHostname}:443`
-);
-
-const CSP = builder({
-  directives: cspPolicy,
-});
-
 module.exports = merge(base, {
   mode: "development",
   devServer: {
@@ -108,10 +55,6 @@ module.exports = merge(base, {
       },
       "/admin": backendHostUrl,
       "/static": backendHostUrl,
-    },
-    headers: {
-      "Reporting-Endpoints": `sentry="${reportUrl.toString()}"`,
-      "Content-Security-Policy": CSP,
     },
   },
   devtool: "inline-cheap-module-source-map",
