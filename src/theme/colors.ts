aad5660421cf2b71045e4c7bec13ec7415b760bb
Parker Henderson <parker@parkerhendo.com>
2022-11-07 09:33:14 -0800

    Updated shadows to be more subtle

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -305,16 +305,16 @@ const semanticColors = {
 const shadows = {
   footer: "0 -2px 1px #0000000d",
   glowDark: `
-    0px 14px 16px hsla(254, 50%, 18%, 8%),
-    0px 7px 9px hsla(254, 50%, 18%, 8%),
-    0px 3px 4px hsla(254, 50%, 18%, 8%),
-    0px -3px 3.25px hsla(254, 50%, 18%, 16%)
+    0px 0.6px 0.7px hsla(254, 50%, 18%, 30%),
+    0px 1.8px 2px hsla(254, 50%, 18%, 30%),
+    0px 4.3px 4.8px -1.7px hsla(254, 50%, 18%, 30%),
+    0px 10.3px 11.6px -2.5px hsla(254, 50%, 18%, 30%)
   `,
   glowLight: `
-    0px 14px 16px hsla(254, 50%, 18%, 2%),
-    0px 7px 9px hsla(254, 50%, 18%, 4%),
-    0px 3px 4px hsla(254, 50%, 18%, 4%),
-    0px -3px 3.25px hsla(254, 50%, 18%, 4%)
+    0px 0.6px 0.7px hsla(254, 30%, 18%, 6%),
+    0px 1.8px 2px hsla(254, 30%, 18%, 2%),
+    0px 4.3px 4.8px -1.7px hsla(254, 30%, 18%, 2%),
+    0px 10.3px 11.6px -2.5px hsla(254, 30%, 18%, 4%)
   `,
   smallGlowDark: `
     0px 12px 15px rgb(80 26 251 / 8%),
