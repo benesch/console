520b312523bc66f3c668bc2e4edf4682cadacb81
Robin Clowers <robin.clowers@gmail.com>
2022-11-10 17:11:11 -0800

    Fix util test

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,21 +1,23 @@
 import { groupBy } from "./util";
 
-test("groupBy", () => {
-  const array = [
-    { k: "a", v: 1 },
-    { k: "b", v: 1 },
-    { k: "a", v: 2 },
-  ];
-  const grouped = groupBy(array, (o) => o.k);
-  expect(grouped).toStrictEqual(
-    new Map(
-      Object.entries({
-        a: [
-          { k: "a", v: 1 },
-          { k: "a", v: 2 },
-        ],
-        b: [{ k: "b", v: 1 }],
-      })
-    )
-  );
+describe("groupBy", () => {
+  it("groupBy", () => {
+    const array = [
+      { k: "a", v: 1 },
+      { k: "b", v: 1 },
+      { k: "a", v: 2 },
+    ];
+    const grouped = groupBy(array, (o) => o.k);
+    expect(grouped).toStrictEqual(
+      new Map(
+        Object.entries({
+          a: [
+            { k: "a", v: 1 },
+            { k: "a", v: 2 },
+          ],
+          b: [{ k: "b", v: 1 }],
+        })
+      )
+    );
+  });
 });
