272e9d6756486839856f182616f7ff5a4ae6afad
Robin Clowers <robin.clowers@gmail.com>
2022-11-17 13:27:15 -0800

    Fix version and org overflows
    Fixes #4413

--- src/version/Version.tsx
+++ src/version/Version.tsx
@@ -7,7 +7,13 @@ import { currentVersion } from "./api";
 const Version = () => {
   if (!isValidString(currentVersion)) return null;
   return (
-    <Tag size="sm" variant="outline" opacity={0.8} fontSize="80%">
+    <Tag
+      size="sm"
+      variant="outline"
+      opacity={0.8}
+      fontSize="80%"
+      css={{ overflowWrap: "anywhere" }}
+    >
       Release {currentVersion}
     </Tag>
   );
