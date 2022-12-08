c8f9531840152986b56a7b94815df1285c00742f
Parker Henderson <parker@parkerhendo.com>
2022-12-08 14:24:12 -0500

    Squares should squares

--- src/svg/CheckmarkIcon.tsx
+++ src/svg/CheckmarkIcon.tsx
@@ -6,8 +6,8 @@ const CheckmarkIcon = () => {
   return (
     <svg
       width="16"
-      height="17"
-      viewBox="0 0 16 17"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
