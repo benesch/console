4cfbbe07dbd41ef5cf847eb56a79f412401940c5
Robin Clowers <robin.clowers@gmail.com>
2022-12-19 14:18:58 -0800

    Add generic error fallback

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -5,6 +5,8 @@
 
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import {
+  Alert,
+  AlertIcon,
   Box,
   BoxProps,
   Center,
@@ -15,12 +17,16 @@ import {
   HStack,
   Spinner,
   StackProps,
+  Text,
   useTheme,
+  VStack,
 } from "@chakra-ui/react";
 import * as CSS from "csstype";
 import * as React from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { NavLink, NavLinkProps } from "react-router-dom";
 
+import SupportLink from "~/components/SupportLink";
 import NavBar from "~/layouts/NavBar";
 import PageFooter from "~/layouts/PageFooter";
 
@@ -67,19 +73,42 @@ export const BaseLayout = ({ overflowY, children }: BaseLayoutProps) => {
           pb={4}
         >
           <Flex flexDir="column" w="full" h="full">
-            <React.Suspense
-              fallback={
-                <Center css={{ height: "100%" }}>
-                  <Spinner />
-                </Center>
-              }
+            <ErrorBoundary
+              FallbackComponent={GenericError}
+              onReset={() => {
+                // reset the state of your app so the error doesn't happen again
+              }}
             >
-              {children}
-            </React.Suspense>
+              <React.Suspense
+                fallback={
+                  <Center css={{ height: "100%" }}>
+                    <Spinner />
+                  </Center>
+                }
+              >
+                {children}
+              </React.Suspense>
+            </ErrorBoundary>
           </Flex>
         </Container>
         <PageFooter />
       </Flex>
+    </Flex>
+  );
+};
+
+export const GenericError = () => {
+  return (
+    <Flex h="100%" w="100%" alignItems="center" justifyContent="center">
+      <VStack spacing={2}>
+        <Alert status="error" rounded="md" p={4} marginTop={2}>
+          <AlertIcon />
+          An unexpected error has occured
+        </Alert>
+        <Text>
+          <SupportLink>Contact support</SupportLink> if the issue persists.
+        </Text>
+      </VStack>
     </Flex>
   );
 };
