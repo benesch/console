1d0bd5e224e4fbf8bce040d612a6d143922fc24c
Robin Clowers <robin.clowers@gmail.com>
2022-12-21 10:02:23 -0800

    Improved date binning
    This very is simpler, plus it aligns the start of the bin to our start
    time, which looks much nicer.

--- src/api/materialized.tsx
+++ src/api/materialized.tsx
@@ -342,17 +342,16 @@ export function useSourceStatuses({
   const result = useSql(
     sourceId
       ? `
-  SELECT * FROM (
-    SELECT count(h.error),
-      (floor(extract(epoch from occurred_at) / ${bucketSizeSeconds}) * ${bucketSizeSeconds}) * 1000 as bin_start
-    FROM mz_internal.mz_source_status_history h
-    WHERE source_id = '${sourceId}'
-    AND h.occurred_at BETWEEN '${startTime.toISOString()}' AND '${endTime.toISOString()}'
-    GROUP BY bin_start
-    ORDER BY bin_start
-  )
-  WHERE bin_start > '${startTime.getTime()}'
-  ORDER BY bin_start;`
+SELECT
+  COUNT(error) count,
+  EXTRACT(epoch FROM date_bin(
+    interval '${bucketSizeSeconds} seconds', occurred_at, '${startTime.toISOString()}'
+    )) * 1000 as bin_start
+FROM mz_internal.mz_source_status_history
+WHERE source_id = '${sourceId}'
+AND occurred_at BETWEEN '${startTime.toISOString()}' AND '${endTime.toISOString()}'
+GROUP BY bin_start
+ORDER BY bin_start DESC;`
       : undefined
   );
   let statuses: SourceErrorBucket[] | null = null;
