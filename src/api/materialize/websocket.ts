a2b7150a0815024f624f5f17cf1dc795c9bdda53
Robin Clowers <robin.clowers@gmail.com>
2023-01-05 11:53:14 -0800

    Extract hook

--- src/api/materialize/websocket.ts
+++ src/api/materialize/websocket.ts
@@ -3,6 +3,8 @@ import { useRecoilValue_TRANSITION_SUPPORT_UNSTABLE } from "recoil";
 
 import { currentEnvironmentState } from "~/recoil/environments";
 
+import { ExplainTimestampResult } from "../materialized";
+
 export interface SimpleRequest {
   query: string;
 }
@@ -80,7 +82,7 @@ export const useSqlWs = (): [
             `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
           );
         } else {
-          // e.g. server process killed or network down
+          // this happens when the client closes the connection, which seems odd
           // event.code is usually 1006 in this case
           console.log("[close] Connection died", event);
         }
@@ -100,4 +102,99 @@ export const useSqlWs = (): [
   }, [currentEnvironment]);
 
   return [socketReady, socketRef];
+};
+
+export interface ReplicaUtilization {
+  id: number;
+  cpuPercent: number;
+  memoryPercent: number;
+}
+
+export const useClusterUtilization = (
+  clusterId: string | undefined,
+  startTime: Date,
+  endTime: Date
+) => {
+  const [results, setResults] = React.useState<ReplicaUtilization[]>([]);
+  const [errors, setErrors] = React.useState<string[]>([]);
+  const [explainSent, setExplainSent] = React.useState<boolean>(false);
+  const [querySent, setQuerySent] = React.useState<boolean>(false);
+  const [minFrontier, setMinFrontier] = React.useState<number | undefined>();
+  const [socketReady, socketRef] = useSqlWs();
+  const utilizationQuery = `SELECT r.id,
+    u.cpu_percent_normalized,
+    u.memory_percent
+  FROM mz_cluster_replicas r
+  JOIN mz_internal.mz_cluster_replica_utilization u ON u.replica_id = r.id
+  WHERE r.cluster_id = '${clusterId}'`;
+
+  React.useEffect(() => {
+    const ws = socketRef.current;
+    if (!ws || !clusterId) return;
+
+    // first we fetch the minimum frontier we can query
+    if (socketReady && !explainSent) {
+      ws.send({
+        query: `EXPLAIN TIMESTAMP AS JSON FOR ${utilizationQuery};`,
+      });
+      setExplainSent(true);
+    }
+    // now we can fetch the utilization data
+    if (socketReady && minFrontier && !querySent) {
+      const frontier = new Date(minFrontier);
+      let start = startTime;
+      if (start < frontier) {
+        start = frontier;
+      }
+      ws.send({
+        query: `SUBSCRIBE (${utilizationQuery})
+  AS OF TIMESTAMP '${start.toISOString()}'
+  UP TO TIMESTAMP '${endTime.toISOString()}';`,
+      });
+      setQuerySent(true);
+    }
+
+    ws.onResult((data) => {
+      if (data.type === "Error") {
+        setErrors((val) => [...val, data.payload]);
+      }
+      if (data.type === "Row") {
+        if (!minFrontier) {
+          const result = JSON.parse(
+            data.payload[0] as string
+          ) as ExplainTimestampResult;
+          setMinFrontier(result.determination.since.elements[0]);
+        } else {
+          const mzdiff = data.payload[1];
+          // ignore retractions
+          if (mzdiff === 1) {
+            setResults((val) => [
+              ...val,
+              {
+                id: data.payload[2] as number,
+                cpuPercent: data.payload[3] as number,
+                memoryPercent: data.payload[4] as number,
+              },
+            ]);
+          }
+        }
+      }
+    });
+
+    ws.socket.onerror = function (event) {
+      console.log("[error]", event);
+    };
+  }, [
+    socketRef,
+    clusterId,
+    socketReady,
+    querySent,
+    explainSent,
+    minFrontier,
+    utilizationQuery,
+    startTime,
+    endTime,
+  ]);
+
+  return { results, errors };
 };
--- src/platform/clusters/ClusterOverview.tsx
+++ src/platform/clusters/ClusterOverview.tsx
@@ -2,101 +2,21 @@ import { Box } from "@chakra-ui/react";
 import { subMinutes } from "date-fns";
 import React from "react";
 
-import { useSqlWs } from "~/api/materialize/websocket";
-import { Cluster, ExplainTimestampResult } from "~/api/materialized";
+import { useClusterUtilization } from "~/api/materialize/websocket";
+import { Cluster } from "~/api/materialized";
 
 export interface Props {
   cluster?: Cluster;
 }
 
-interface ReplicaInfo {
-  id: number;
-  cpuPercent: number;
-  memoryPercent: number;
-}
-
 const ClusterOverview = ({ cluster }: Props) => {
-  const [results, setResults] = React.useState<ReplicaInfo[]>([]);
-  const [explainSent, setExplainSent] = React.useState<boolean>(false);
-  const [querySent, setQuerySent] = React.useState<boolean>(false);
-  const [minFrontier, setMinFrontier] = React.useState<number | undefined>();
-  const [socketReady, socketRef] = useSqlWs();
-  const utilizationQuery = `SELECT r.id,
-    u.cpu_percent_normalized,
-    u.memory_percent
-  FROM mz_cluster_replicas r
-  JOIN mz_internal.mz_cluster_replica_utilization u ON u.replica_id = r.id
-  WHERE r.cluster_id = '${cluster?.id}'`;
+  const endTime = React.useMemo(() => new Date(), []);
+  const [timePeriodMinutes, _setTimePeriodMinutes] = React.useState(60);
+  const startTime = React.useMemo(() => {
+    return subMinutes(endTime, timePeriodMinutes);
+  }, [timePeriodMinutes, endTime]);
 
-  React.useEffect(() => {
-    const ws = socketRef.current;
-    if (!ws) return;
-
-    // first we fetch the minimum frontier we can query
-    if (socketReady && cluster && !explainSent) {
-      ws.send({
-        query: `EXPLAIN TIMESTAMP AS JSON FOR ${utilizationQuery};`,
-      });
-      setExplainSent(true);
-    }
-    // now we can fetch the utilization data
-    if (socketReady && cluster && minFrontier && !querySent) {
-      const frontier = new Date(minFrontier);
-      const endTime = new Date();
-      let startTime = subMinutes(endTime, 1440);
-      if (startTime < frontier) {
-        startTime = frontier;
-      }
-      ws.send({
-        query: `SUBSCRIBE (${utilizationQuery})
-  AS OF TIMESTAMP '${startTime.toISOString()}'
-  UP TO TIMESTAMP '${endTime.toISOString()}';`,
-      });
-      setQuerySent(true);
-    }
-
-    ws.onResult((data) => {
-      if (data.type === "Error") {
-        console.log("error", data.payload);
-      }
-      if (data.type === "Rows") {
-        console.log("columns", data.payload);
-      }
-      if (data.type === "Row") {
-        if (!minFrontier) {
-          const result = JSON.parse(
-            data.payload[0] as string
-          ) as ExplainTimestampResult;
-          setMinFrontier(result.determination.since.elements[0]);
-        } else {
-          const mzdiff = data.payload[1];
-          // ignore retractions
-          if (mzdiff === 1) {
-            setResults((val) => [
-              ...val,
-              {
-                id: data.payload[2] as number,
-                cpuPercent: data.payload[3] as number,
-                memoryPercent: data.payload[4] as number,
-              },
-            ]);
-          }
-        }
-      }
-    });
-
-    ws.socket.onerror = function (event) {
-      console.log("[error]", event);
-    };
-  }, [
-    socketRef,
-    cluster,
-    socketReady,
-    querySent,
-    explainSent,
-    minFrontier,
-    utilizationQuery,
-  ]);
+  const { results } = useClusterUtilization(cluster?.id, startTime, endTime);
 
   return (
     <Box>
