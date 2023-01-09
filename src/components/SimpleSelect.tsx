0f5a2c69efa48f460a6bda91758e47bec769fc84
Parker Henderson <parker@parkerhendo.com>
2023-01-09 09:47:34 -0800

    Explicitly set height to 32px

--- src/components/SimpleSelect.tsx
+++ src/components/SimpleSelect.tsx
@@ -12,13 +12,14 @@ const SimpleSelect = (props: SimpleSelectProps) => {
       fontSize="14px"
       lineHeight="16px"
       fontFamily="Inter"
+      height="32px"
       width="auto"
       rounded="8px"
       borderWidth="1px"
       boxShadow="
         0px 1px 3px 0px hsla(0, 0%, 0%, 0.06), 
         0px 1px 1px 0px hsla(0, 0%, 0%, 0.04),
-        0px 0px 0px 0px hsla(0, 0%, 0%, 0)" // for performance while transitioning
+        0px 0px 0px 0px hsla(0, 0%, 0%, 0)"
       transition="box-shadow 50ms ease-out"
       sx={{
         _hover: {
