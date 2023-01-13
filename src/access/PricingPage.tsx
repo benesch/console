1655a5837a85f66b21e9a40d16652e700be904cb
Robin Clowers <robin.clowers@gmail.com>
2023-01-13 08:50:44 -0800

    Simplify pricing for unified cluster billing

--- src/access/PricingPage.tsx
+++ src/access/PricingPage.tsx
@@ -6,7 +6,6 @@ import {
   Table,
   Tbody,
   Td,
-  Text,
   Th,
   Thead,
   Tr,
@@ -31,7 +30,7 @@ const showFronteggSubscriptionPortal = () => {
 
 const PricingPage = () => {
   const { colors } = useTheme<MaterializeTheme>();
-  const { pricingTerms, consumptionTables, regions, terms } = data;
+  const { pricingTerms, consumptionTables, regions } = data;
   const [region, setRegion] = React.useState<StaticRegion>(
     regions[0] as StaticRegion
   );
@@ -119,10 +118,6 @@ const PricingPage = () => {
           </Table>
           <CardContent>
             <VStack spacing={4} alignItems="stretch" fontSize="sm">
-              <Text>
-                Free Network Egress up to 1TB for a limited time during Early
-                Access*
-              </Text>
               <HStack justifyContent="flex-start" pt={2}>
                 <Button
                   variant="gradient-1"
@@ -165,11 +160,6 @@ const PricingPage = () => {
                 </Table>
               ))}
             </VStack>
-          </CardContent>
-        </Card>
-        <Card minWidth="fit-content">
-          <CardContent>
-            <Text fontSize="xs">*{terms}</Text>
           </CardContent>
         </Card>
       </VStack>
