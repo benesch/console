b1608d6bba89608a7ba41abb7d5e7acbbc65722d
Robin Clowers <robin.clowers@gmail.com>
2023-01-20 15:08:51 -0800

    Remove unused petnames code

--- src/util.ts
+++ src/util.ts
@@ -3,8 +3,6 @@
  * Generic utility functions.
  */
 
-import petnames from "../../config/petnames.json";
-
 /**
  * @description
  * Takes an Array<V>, and a grouping function,
@@ -47,21 +45,6 @@ export function assert(condition: any): asserts condition {
 /** Choose a random element from an array. */
 export function randomChoice<T>(array: Array<T>) {
   return array[Math.floor(Math.random() * array.length)];
-}
-
-/**
- * Generate a "pet name".
- *
- * A pet name consists of an adjective and animal name, separated by a hyphen,
- * and all lowercase. They are useful as unique handles that are human readable.
- *
- * If you change this function, change the Python implementation in
- * console.util.nameutil to match.
- * */
-export function petname() {
-  const adjective = randomChoice(petnames["adjectives"]);
-  const animal = randomChoice(petnames["animals"]);
-  return `${adjective}-${animal}`;
 }
 
 /** Create a promise that resolves after the specified number of milliseconds.
