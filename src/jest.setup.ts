7847bed6faab4e16c00023dc063adcee6ba31672
Robin Clowers <robin.clowers@gmail.com>
2023-02-09 10:31:42 -0800

    Add msw debugging

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,15 +1,30 @@
 import "@testing-library/jest-dom";
 
+import debug from "debug";
+
 import server from "./api/mocks/server";
 
+const debugMz = debug("mz");
+
 // Establish API mocking before all tests.
-beforeAll(() => server.listen());
+beforeAll(() => {
+  if (debugMz.enabled) {
+    server.events.on("request:start", (req) => {
+      debugMz(`[request] ${req.method}, ${req.url.href}`, req.body);
+    });
+  }
+
+  server.listen();
+});
 
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers());
 
 // Clean up after the tests are finished.
-afterAll(() => server.close());
+afterAll(() => {
+  server.events.removeAllListeners();
+  server.close();
+});
 
 export {};
