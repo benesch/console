0f2670d0b0174620146b01e60469c3dcae004b62
Robin Clowers <robin.clowers@gmail.com>
2023-02-09 19:38:49 -0800

    Generate app passwords inline

--- src/components/ConnectModal.tsx
+++ src/components/ConnectModal.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Flex,
   HStack,
   Modal,
   ModalBody,
@@ -8,15 +9,21 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Spinner,
   Text,
   useDisclosure,
   useTheme,
+  VStack,
 } from "@chakra-ui/react";
 import React from "react";
 
 import ConnectSteps from "~/platform/home/ConnectSteps";
 import { MaterializeTheme } from "~/theme";
+import useAppPasswords, {
+  NEW_USER_DEFAULT_PASSWORD_NAME,
+} from "~/useAppPasswords";
 
+import { CopyableBox } from "./copyableComponents";
 import SupportLink from "./SupportLink";
 import TextLink from "./TextLink";
 
@@ -52,22 +59,7 @@ const ConnectModal = () => {
             </Text>
             <ConnectSteps mt="4" />
             <Box mt="6">
-              <HStack justifyContent="space-between">
-                <Box>
-                  <Text fontSize="sm" fontWeight="500">
-                    Create an app password
-                  </Text>
-                  <Text
-                    fontSize="sm"
-                    color={semanticColors.foreground.secondary}
-                  >
-                    Create a new app password if you don’t have one accessible.
-                  </Text>
-                </Box>
-                <Button as="a" href="/access" variant="primary" size="sm">
-                  Create app password
-                </Button>
-              </HStack>
+              <CreateAppPassword />
             </Box>
             <Box
               mt="8"
@@ -89,6 +81,93 @@ const ConnectModal = () => {
           </ModalBody>
         </ModalContent>
       </Modal>
+    </>
+  );
+};
+
+const CreateAppPassword = () => {
+  const {
+    colors: { semanticColors },
+  } = useTheme<MaterializeTheme>();
+  const {
+    addUserApiToken,
+    loadingInProgress,
+    createInProgress,
+    tokensState,
+    newPassword,
+  } = useAppPasswords();
+
+  if (loadingInProgress) {
+    return (
+      <Flex alignItems="center" color={semanticColors.foreground.secondary}>
+        <Spinner size="sm" mr={2} /> Loading...
+      </Flex>
+    );
+  }
+
+  if (createInProgress) {
+    return (
+      <Flex alignItems="center" color={semanticColors.foreground.secondary}>
+        <Spinner size="sm" mr={2} /> Generating new app password...
+      </Flex>
+    );
+  }
+
+  if (newPassword) {
+    return (
+      <>
+        <VStack alignItems="stretch">
+          <Text
+            as="span"
+            fontSize="sm"
+            lineHeight="16px"
+            fontWeight={500}
+            color={semanticColors.foreground.primary}
+          >
+            New app password:
+          </Text>
+          <CopyableBox contents={newPassword}>{newPassword}</CopyableBox>
+        </VStack>
+        <Text
+          pt={3}
+          fontSize="sm"
+          lineHeight="20px"
+          fontWeight={400}
+          color={semanticColors.foreground.primary}
+        >
+          Copy this app password somewhere safe. App passwords cannot be
+          displayed after initial creation.
+        </Text>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <HStack justifyContent="space-between">
+        <Box>
+          <Text fontSize="sm" fontWeight="500">
+            Create an app password
+          </Text>
+          <Text fontSize="sm" color={semanticColors.foreground.secondary}>
+            Create a new app password if you don’t have one accessible.
+          </Text>
+        </Box>
+        <Button
+          onClick={() =>
+            addUserApiToken({
+              description: `${NEW_USER_DEFAULT_PASSWORD_NAME} ${
+                tokensState.apiTokensDataUser.length + 1
+              }`,
+            })
+          }
+          disabled={!!(tokensState.successDialog.secret || createInProgress)}
+          variant="primary"
+          size="sm"
+        >
+          Create app password
+        </Button>
+      </HStack>
     </>
   );
 };
