061b4eb114032471c7f6d247c8e933b8d67205d1
Graham Gnall <graham@materialize.com>
2023-02-15 14:26:45 -0500

    added csr secrets in addition to kafka ones

--- src/platform/home/GettingStarted.tsx
+++ src/platform/home/GettingStarted.tsx
@@ -18,7 +18,9 @@ import { MaterializeTheme } from "~/theme";
 
 // These credentials are read only and not considered sensitive
 const secrets = `CREATE SECRET qck.kafka_user AS 'CL6M5VSYI32TVILA';
-CREATE SECRET qck.kafka_password AS 'swK5gpo9J3uJKaeeHjTkKXnU7qd5Gp90FDJq4CbHKvNnU4kl7uQ1jzVIGsvhHB0K';`;
+CREATE SECRET qck.kafka_password AS 'swK5gpo9J3uJKaeeHjTkKXnU7qd5Gp90FDJq4CbHKvNnU4kl7uQ1jzVIGsvhHB0K';
+CREATE SECRET qck.csr_user AS 'DISCU3R3EBELNOZQ';
+CREATE SECRET qck.csr_password AS 'pQwNVqWdGs8P4VUpdYYoHfnpc0B1lqXTmZKD+U3O/yh+vMrAj4jDwTAbHuzSlkei';`;
 
 const handleGettingStartedClick = () => {
   segment.track("Get Started Clicked", {});
