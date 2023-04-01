c344f4f64daa1ea3d2c877988b243d5b8a27719d
Brennan Vincent <brennan@umanwizard.com>
2023-04-01 09:21:11 -0600

    [useDataflowStructure] update to deal with introspection schema changes

--- src/api/materialize/useDataflowStructure.ts
+++ src/api/materialize/useDataflowStructure.ts
@@ -53,7 +53,7 @@ export function useDataflowStructure(params?: DataflowStructureParams) {
         {
           query: `CREATE TEMPORARY VIEW export_to_dataflow AS
   SELECT export_id, id FROM mz_internal.mz_compute_exports AS mce JOIN mz_internal.mz_dataflows AS md ON
-  mce.dataflow_id = md.local_id WHERE mce.worker_id = 0 AND md.worker_id = 0`,
+  mce.dataflow_id = md.local_id`,
           params: [],
         },
         // OPERATORS
@@ -71,12 +71,7 @@ export function useDataflowStructure(params?: DataflowStructureParams) {
                 LEFT JOIN mz_internal.mz_arrangement_sizes
                                 AS mas ON mdod.id = mas.operator_id
                 LEFT JOIN mz_internal.mz_dataflow_operator_parents
-                                AS mdop ON mdod.id = mdop.id
-	WHERE
-		mdod.worker_id = 0
-                AND coalesce(mdop.worker_id, 0) = 0
-                AND coalesce(mse.worker_id, 0) = 0
-                AND coalesce(mas.worker_id, 0) = 0`,
+                                AS mdop ON mdod.id = mdop.id`,
           params: [],
         },
         { query: "BEGIN", params: [] },
@@ -96,8 +91,7 @@ FROM
         JOIN mz_internal.mz_dataflow_operator_dataflows mdod ON from_operator_id = mdod.id
         JOIN export_to_dataflow e2d ON e2d.id = mdod.dataflow_id
 WHERE
-	mdco.worker_id = 0
-AND e2d.export_id = $1
+    e2d.export_id = $1
 	AND (
 			EXISTS(
 				SELECT
@@ -144,14 +138,14 @@ GROUP BY
         id: x("id") as number,
         name: x("name"),
         parentId: x("parent_id") as number | null,
-        arrangementRecords: x("arrangement_records") as number,
+        arrangementRecords: +(x("arrangement_records") as string),
         elapsedNs: x("elapsed_ns") as number,
       }));
       const channels = extractData(channelsData, (x) => ({
         id: x("id") as number,
         fromOperatorId: x("from_operator_id") as number,
         toOperatorId: x("to_operator_id") as number,
-        messagesSent: x("sent") as number,
+        messagesSent: +(x("sent") as string),
       }));
       return { operators, channels };
     } else {
