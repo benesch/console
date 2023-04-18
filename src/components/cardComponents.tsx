e361cd25be5f5c6da6701e2136443473b08f4bb7
Robin Clowers <robin.clowers@gmail.com>
2023-04-18 16:52:32 -0700

    Remove unused card components

--- src/components/cardComponents.tsx
+++ src/components/cardComponents.tsx
@@ -3,9 +3,8 @@
  * A reusable "card" component.
  */
 
-import { BoxProps, Flex, HeadingProps } from "@chakra-ui/layout";
-import { Box, Heading, HStack, Text, useTheme } from "@chakra-ui/react";
-import { Tab, TabList, TabListProps, TabProps, Tabs } from "@chakra-ui/tabs";
+import { BoxProps, HeadingProps } from "@chakra-ui/layout";
+import { Box, Heading, useTheme } from "@chakra-ui/react";
 import React from "react";
 
 import { MaterializeTheme } from "~/theme";
@@ -13,23 +12,16 @@ import { MaterializeTheme } from "~/theme";
 export const CARD_PADDING = 4;
 
 /**
- * A "card" UI element with a header, body, and footer.
- *
+ * A "card" UI element with a header, body.
+ React.PropsWithChildren
  * Example usage:
  *
  * ```
  * <Card>
  *   <CardHeader>Card title</CardHeader>
  *   <CardContent>
- *     <VStack spacing="3" align="left">
- *       <CardField name="Property 1">Value 1</CardField>
- *       <CardField name="Property 2">Value 2</CardField>
- *     </VStack>
+ *     Some content
  *   </CardContent>
- *   <CardFooter>
- *     <Spacer />
- *     <Button>Take some action</Button>
- *   </CardFooter>
  * </Card>
  * ```
  */
@@ -58,7 +50,7 @@ export interface CardTitleProps extends HeadingProps {
   children: React.ReactNode;
 }
 
-/* A title for a `Card`. Used standalone in `CardTabsHeaders.` */
+/* A title for a `Card`. Used standalone in `CardHeader.` */
 export const CardTitle = (props: CardTitleProps) => {
   return <Heading fontSize="lg" fontWeight="600" p={CARD_PADDING} {...props} />;
 };
@@ -75,62 +67,11 @@ export const CardHeader = (props: CardTitleProps) => {
   );
 };
 
-export const CardTabs = Tabs;
-
-export const CardTab: React.FC<React.PropsWithChildren<TabProps>> = (props) => {
-  return <Tab {...props} />;
-};
-
-/** a drop in replacement to the TabList component that can be used in a card header container */
-export const CardTabsHeaders: React.FC<
-  React.PropsWithChildren<TabListProps>
-> = (props) => {
-  return (
-    <TabList as={Flex} display="flex" alignItems="center" {...props}></TabList>
-  );
-};
-
 /** The container of the body content for a `Card`. */
 export const CardContent = ({ children, ...props }: BoxProps) => {
   return (
     <Box p={CARD_PADDING} {...props}>
       {children}
-    </Box>
-  );
-};
-
-export interface CardFooterProps {
-  children?: React.ReactNode;
-}
-
-/** A footer for a `Card`. */
-export const CardFooter = (props: CardFooterProps) => {
-  const { colors } = useTheme<MaterializeTheme>();
-  return (
-    <HStack
-      display="flex"
-      borderTop="1px"
-      borderTopColor={colors.semanticColors.border.primary}
-      p={CARD_PADDING}
-    >
-      {props.children}
-    </HStack>
-  );
-};
-
-export interface CardFieldProps {
-  name: string;
-  children: React.ReactNode;
-}
-
-/** A name–value pair for display in a `Card`. */
-export const CardField = (props: CardFieldProps) => {
-  return (
-    <Box data-field-name={props.name}>
-      <Text fontSize="sm" color="grey">
-        {props.name}
-      </Text>
-      {props.children}
     </Box>
   );
 };
