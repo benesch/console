6708526a72e57d474d6b1308d6024f486ab65d0b
Robin Clowers <robin.clowers@gmail.com>
2023-06-05 08:32:29 -0700

    Simplify useSources query building
    According to the docs:
    https://kysely-org.github.io/kysely/classes/SelectQueryBuilder.html#_if
    The $if function creates additional typescript overhead, so it should
    only be used for selects, where it actually affects the result type.

--- src/api/materialize/useSources.ts
+++ src/api/materialize/useSources.ts
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { assert } from "~/util";
-
 import { queryBuilder } from "./db";
 import useSqlTyped from "./useSqlTyped";
 
@@ -14,7 +12,7 @@ function useSources({
   nameFilter,
 }: { databaseId?: string; schemaId?: string; nameFilter?: string } = {}) {
   const query = React.useMemo(() => {
-    return queryBuilder
+    const qb = queryBuilder
       .selectFrom("mz_catalog.mz_sources as s")
       .select(["s.id", "s.name", "s.type", "s.size"])
       .innerJoin("mz_catalog.mz_schemas as sc", "sc.id", "s.schema_id")
@@ -24,20 +22,17 @@ function useSources({
       .leftJoin("mz_internal.mz_source_statuses as st", "st.id", "s.id")
       .select(["st.status", "st.error"])
       .where("s.id", "like", "u%")
-      .where("s.type", "<>", "subsource")
-      .$if(!!databaseId, (qb) => {
-        assert(databaseId);
-        return qb.where("d.id", "=", databaseId);
-      })
-      .$if(!!schemaId, (qb) => {
-        assert(schemaId);
-        return qb.where("sc.id", "=", schemaId);
-      })
-      .$if(!!nameFilter, (qb) => {
-        assert(nameFilter);
-        return qb.where("s.name", "like", `%${nameFilter}%`);
-      })
-      .compile();
+      .where("s.type", "<>", "subsource");
+    if (databaseId) {
+      qb.where("d.id", "=", databaseId);
+    }
+    if (schemaId) {
+      qb.where("sc.id", "=", schemaId);
+    }
+    if (nameFilter) {
+      qb.where("s.name", "like", `%${nameFilter}%`);
+    }
+    return qb.compile();
   }, [databaseId, nameFilter, schemaId]);
 
   const sourceResponse = useSqlTyped(query);
