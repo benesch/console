93802fd0e389734283f1ffd3fabbd721d8772c2a
Sang Jun Bak <sangjun.bak99@gmail.com>
2023-06-21 10:00:00 -0400

    Add custom theme provider for shell dark mode

--- src/layouts/ShellLayout.tsx
+++ src/layouts/ShellLayout.tsx
@@ -1,16 +1,9 @@
-import {
-  Box,
-  Center,
-  Flex,
-  Spinner,
-  ThemeProvider,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Center, Flex, Spinner, toCSSVar, VStack } from "@chakra-ui/react";
+import { Global, ThemeProvider as EmotionThemeProvider } from "@emotion/react";
 import { ErrorBoundary } from "@sentry/react";
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useMemo } from "react";
 
 import AccountStatusAlert from "~/components/AccountStatusAlert";
-import EnvironmentError from "~/components/EnvironmentError";
 import ErrorBox from "~/components/ErrorBox";
 import { darkTheme } from "~/theme";
 
@@ -20,6 +13,21 @@ import PageFooter from "./PageFooter";
 type ShellLayoutProps = PropsWithChildren;
 
 const MAIN_CONTENT_MARGIN = 4;
+
+/**
+ * Theme provider so that everything within Shell is in dark mode.
+ * Can't reuse Chakra's ThemeProvider since the CSS variables would override the overall app theme's CSS variables.
+ * Derived from https://github.com/chakra-ui/chakra-ui/blob/main/packages/core/system/src/providers.tsx
+ */
+export const ShellThemeProvider = ({ children }: PropsWithChildren) => {
+  const computedTheme = useMemo(() => toCSSVar(darkTheme), []);
+  return (
+    <EmotionThemeProvider theme={computedTheme}>
+      <Global styles={(theme: any) => ({ "#shell": theme.__cssVars })} />
+      {children}
+    </EmotionThemeProvider>
+  );
+};
 
 /**
  * The layout for shell, containing the navigation bar at the
@@ -50,32 +58,31 @@ export const ShellLayout = (props: ShellLayoutProps) => {
         <NavBar />
 
         <VStack flex={1} alignItems="stretch" spacing={0}>
-          <ThemeProvider theme={darkTheme}>
-            <Box
-              flex={1}
-              as="main"
-              m={MAIN_CONTENT_MARGIN}
-              bg="semanticColors.background.primary"
-              minHeight="0"
-              borderRadius="lg"
+          <ErrorBoundary fallback={<ErrorBox />}>
+            <React.Suspense
+              fallback={
+                <Center css={{ height: "100%" }}>
+                  <Spinner />
+                </Center>
+              }
             >
-              <Flex flexDir="column" w="100%" h="100%">
-                <ErrorBoundary fallback={<ErrorBox />}>
-                  <React.Suspense
-                    fallback={
-                      <Center css={{ height: "100%" }}>
-                        <Spinner />
-                      </Center>
-                    }
-                  >
-                    <EnvironmentError hideContentOnEnvironmentError>
-                      {props.children}
-                    </EnvironmentError>
-                  </React.Suspense>
-                </ErrorBoundary>
-              </Flex>
-            </Box>
-          </ThemeProvider>
+              <ShellThemeProvider>
+                <Box
+                  as="main"
+                  m={MAIN_CONTENT_MARGIN}
+                  bg="semanticColors.background.primary"
+                  borderRadius="lg"
+                  id="shell"
+                  flex="1"
+                  style={{
+                    boxShadow: "none", // We set box shadow to none since the theme provider sets its box shadow by default
+                  }}
+                >
+                  {props.children}
+                </Box>
+              </ShellThemeProvider>
+            </React.Suspense>
+          </ErrorBoundary>
           <PageFooter />
         </VStack>
       </Flex>
